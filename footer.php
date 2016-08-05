cba3389fa151a59d6bead58126db91f8ddb17965
Dearyuer <may.dearyuer@gmail.com>
2016-08-05 15:43:17 +0800

    updates

--- footer.php
+++ footer.php
@@ -2,7 +2,30 @@
 	
 		<div class="footer">
 			<footer class="container">
-				<p class="footer-inner"><?php bloginfo('name'); ?> - &copy; <?php echo date('Y') ?></p>
+				
+				<ul class="footer-links right">
+					<li>
+						<p><a href="//codrips.com"><?php _e("Activity"); ?></a></p>
+					</li>
+					<li>
+						<p><a href="//codrips.com"><?php _e("Archive"); ?></a></p>
+					</li>
+					<li>
+						<p><a href="//codrips.com"><?php _e("About"); ?></a></p>
+					</li>
+				</ul>
+				<a href="<?php echo home_url(); ?>" class="footer-heart">
+					<i class="fa fa-heart" aria-hidden="true"></i>
+				</a>
+				<ul class="footer-links">
+					<li><p class="footer-inner"><?php bloginfo('name'); ?> - &copy; <?php echo date('Y') ?></p></li>
+					<li>
+						<p><a href="//codrips.com"><?php _e("Theme"); ?></a></p>
+					</li>
+					<li>
+						<p><a href="//codrips.com"><?php _e("License"); ?></a></p>
+					</li>
+				</ul>
 			</footer>
 		</div>
 	<!-- make sure this is the last line -->
