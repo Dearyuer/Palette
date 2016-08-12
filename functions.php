9f3fed937f796502994f12fb512b0c3eab159a4b
Dearyuer <may.dearyuer@gmail.com>
2016-08-12 14:13:06 +0800

    updates

--- functions.php
+++ functions.php
@@ -220,31 +220,36 @@ function mytheme_comment($comment, $args, $depth) {
         <div id="div-comment-<?php comment_ID() ?>" class="comment-body">
     <?php endif; ?>
     <div class="comment-author vcard">
+      <div class="comment-author-avatar">
         <?php if ( $args['avatar_size'] != 0 ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-        <?php printf( __( '<cite class="fn">%s</cite> <span class="says">says:</span>' ), get_comment_author_link() ); ?>
+      </div>
+      <div class="comment-info">
+        <?php printf( __( '<div class="fn"><div class="comment-author-link">%s</div>' ), get_comment_author_link() ); ?>
+        <div class="comment-meta commentmetadata">
+            <span>
+            <?php
+            /* translators: 1: date, 2: time */
+            printf( __('<i class="fa fa-clock-o" aria-hidden="true"></i> %1$s'), timeAgo(time(),get_comment_time("U"))); 
+            ?>
+            </span>
+            <?php if ( $comment->comment_approved == '0' ) : ?>
+                 <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em>
+                  <br />
+            <?php endif; ?>
+            <?php edit_comment_link( __( '(Edit)' ), '  ', '' );
+            ?>
+        </div>
+        <?php echo '</div>';//end fn ?>
+      </div>
+      <div class="reply">
+          <?php comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+      </div>
     </div>
-    <?php if ( $comment->comment_approved == '0' ) : ?>
-         <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em>
-          <br />
-    <?php endif; ?>
 
-    <div class="comment-meta commentmetadata">
-        <span>
-        <?php
-        /* translators: 1: date, 2: time */
-        printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time() ); 
-        ?>
-        </span>
-
-        <?php edit_comment_link( __( '(Edit)' ), '  ', '' );
-        ?>
+    <div class="comment-text">
+      <?php comment_text(); ?>
     </div>
 
-    <?php comment_text(); ?>
-
-    <div class="reply">
-        <?php comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-    </div>
     <?php if ( 'div' != $args['style'] ) : ?>
     </div>
     <?php endif; ?>
