2b33158440f7411e42b0ae5c52c4ae7ab0add93d
Dearyuer <may.dearyuer@gmail.com>
2016-08-26 11:37:37 +0800

    init

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 			<h2 class="title"><?php bloginfo('name'); ?></h2>
 			<p class="description"><?php bloginfo('description'); ?></p>
 			<a href="<?php echo get_home_url().'/blog' ?>"><button class="btn">Go to blog</button></a>
-		</div>	
-		<div class="latest-post">
-			<div class="component-title"><p><i class="fa fa-sticky-note" aria-hidden="true"></i> Latest post</p></div>
+        </div>
+        <div class="latest-content">
 			<div class="row">
 				<div class="pallete-col-2 content">
+                    <div class="component-title"><p>Latest post</p></div>
 					<div>
 						<?php 
 						$home_latest_post = new WP_Query('posts_per_page=1');
@@ -17,10 +17,7 @@
 						if($home_latest_post->have_posts()){
 							while($home_latest_post->have_posts()){
 								$home_latest_post->the_post();
-
 								$home_latest_post_title = get_the_title();
-
-
 								?>
 								<p><?php the_excerpt(); ?></p>
 								<?php
@@ -30,64 +27,12 @@
 						?>
 					</div>
 				</div>
-				<div class="pallete-col-2">
-					<div class="inner">
-						<div class="sticky-note"><i class="fa fa-sticky-note-o" aria-hidden="true"></i></div>
-						<div class="title"><?php echo $home_latest_post_title; ?></div>
-					</div>
-
-				</div>
-			</div>
-		</div>
-
-		<div class="latest-post latest-comment">
-			<div class="component-title"><p><i class="fa fa-comment" aria-hidden="true"></i> Latest comment</p></div>
-			<div class="row">
-				<div class="pallete-col-2">
-					<div class="inner">
-						<div class="sticky-note"><i class="fa fa-comment-o" aria-hidden="true"></i></div>
-						<!-- <div class="title"><?php //echo $home_latest_post_title; ?></div> -->
-					</div>
-				</div>
-				<div class="pallete-col-2 content right">
-					<div>
-						<?php 
-						$comments = get_comments(array(
-							'number'      => 1,
-							'status'      => 'approve',
-							'post_status' => 'publish'
-						));
-						foreach((array)$comments as $comment){
-							echo '<p>'.$comment->comment_content.'</p><br/>';
-						}
-						
-						?>
-					</div>
-				</div>
-				
-			</div>
-		</div>
-		<div class="latest-post">
-			<div class="component-title"><p><i class="fa fa-code" aria-hidden="true"></i> Contribution</p></div>
-			<div class="row">
-				
 				<div class="pallete-col-2 content">
-					<div>
-						<p>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-						</p>
-					</div>
+                    <div class="component-title"><p>Lastest comments</p></div>
 				</div>
-				<div class="pallete-col-2">
-					<div class="inner">
-						<div class="sticky-note"><i class="fa fa-code" aria-hidden="true"></i></div>
-						<!-- <div class="title"><?php //echo $home_latest_post_title; ?></div> -->
-					</div>
-				</div>
-				
 			</div>
 		</div>
-		<div class="footer-info ">
+        <div class="footer-info ">
 			<button class="btn"><i class="fa fa-angle-up" aria-hidden="true"></i></button>
 		</div>	
 
