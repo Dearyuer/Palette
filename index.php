9283fda7bcad16e7d0c022c1a3070711a47faa45
Dearyuer <may.dearyuer@gmail.com>
2016-10-22 14:48:35 +0800

    updates

--- index.php
+++ index.php
@@ -12,59 +12,16 @@
                 </div>
             </div>
         </div>
+        <div class="row home-gallery">
+            <div class="home-grid">1</div>
+            <div class="home-grid">2</div>
+            <div class="home-grid">3</div>
+            <div class="home-grid">4</div>
+            <div class="home-grid">5</div>
 
-        <div class="row">
-                <div class="home-component">
-                <div class="component-identifier"><span class="home-posts-icon"><i class="fa fa-file-text" aria-hidden="true"></i></span></div>
-                <div class="content">
-                    <div class="component-title"><p>Latest post</p></div>
-                    <div class="component-content">
-                    <?php 
-                    $home_latest_post = new WP_Query('posts_per_page=1');
-                    $home_latest_post_title = '';
-                    if($home_latest_post->have_posts()){
-                        while($home_latest_post->have_posts()){
-                            $home_latest_post->the_post();
-                            $home_latest_post_title = get_the_title();
-                            ?>
-                            <p><?php the_excerpt(); ?></p>
-                            <?php
-                        }
-                    }
-                    wp_reset_postdata();
-                    ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-                <div class="home-component">
-                    <div class="component-identifier"><i class="fa fa-comments" aria-hidden="true"></i></div>
-                    <div class="content">
-                        <div class="component-title"><p>Lastest comments</p></div>
-                        <ul class="home-recent-comments">
-                        <?php 
-                        $comments = get_comments(array(
-                            'number'      => 3,
-                            'status'      => 'approve',
-                            'post_status' => 'publish'
-                        ));
-                        foreach((array)$comments as $comment){
-                            echo '<li>';
-                            // echo '<div class="comment-avatar float-left clearfix"><a href="'.$comment->comment_author_url.'">'.get_avatar($comment,40).'</a></div>';
-                            echo '<div class="comment-author float-left clearfix">'.substr($comment->comment_author,0,1).'</div>';
-                                echo '<p class="home-comments">'.$comment->comment_content.'</p>';
-                            echo '</li>';
-                        }
-                        
-                        ?>
-                        </ul>
-                    </div>
-                </div>
         </div>
 
 
-        <?php dynamic_sidebar('home_page'); ?>
         <div class="footer-info ">
 			<button class="btn"><i class="fa fa-angle-up" aria-hidden="true"></i></button>
 		</div>	
