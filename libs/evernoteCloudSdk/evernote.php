bcea4ad0a7eb4142201d8cd98cc6b1578d2161f3
Dearyuer <may.dearyuer@gmail.com>
2016-12-01 14:48:04 +0800

    updates

--- libs/evernoteCloudSdk/evernote.php
+++ libs/evernoteCloudSdk/evernote.php
@@ -11,21 +11,42 @@
 	$getResourceBody = true;
 	$getResourceOCRData = true;
 	$getResourceAlternateData = true;
-	$results = $client->findNotesWithSearch('');
+	// final class NoteSortOrder {
+	//   const CREATED = 1;
+	//   const UPDATED = 2;
+	//   const RELEVANCE = 3;
+	//   const UPDATE_SEQUENCE_NUMBER = 4;
+	//   const TITLE = 5;
+	//   static public $__names = array(
+	//     1 => 'CREATED',
+	//     2 => 'UPDATED',
+	//     3 => 'RELEVANCE',
+	//     4 => 'UPDATE_SEQUENCE_NUMBER',
+	//     5 => 'TITLE',
+	//   );
+	// }
+	// public function findNotesWithSearch($noteSearch, Notebook $notebook = null, $scope = 0, $sortOrder = 0, $maxResults = 20)
+	$results = $client->findNotesWithSearch('',null,0,2,5);
 	$noteGuid;
-	$notes = [];		
+	$notes = [];
 
 
 	$withData = true;
 	$withRecognition = true;
 	$withAlternateData = true;
+	// $loopIndex = 0;
 	foreach ($results as $note) {
+		// if($loopIndex == 5){
+		// 	break;
+		// }
 	    $noteGuid    = $note->guid;
 	    $noteType    = $note->type;
 	    $noteTitle   = $note->title;
 	    $noteCreated = $note->created;
 	    $noteUpdated = $note->updated;
 	    //$str = $noteStore->getNoteContent($token, $noteGuid);
+	    // public function getNoteContent($authenticationToken, $guid);
+	    // $note = $noteStore->getNoteContent($token, $noteGuid);
         $note = $noteStore->getNote($token,$noteGuid,$getNoteContent,$getResourceBody,$getResourceOCRData,$getResourceAlternateData);
 
         //$contentHash = 'da27ddc619e60d0c8d1eab981c525999';
@@ -34,7 +55,9 @@
 	    // var_dump($match[0]);
 	    //$content = str_replace('<en-note>', '', $match[0]);
 	    //echo $content;
+
         array_push($notes,$note);
+        // $loopIndex++;
 	}
 	echo json_encode($notes);
  ?>
